fb95e80b93c719826026bcfbe0d42b8edae73dca
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-04-17 20:21:38 -0300

    chore: add header in homepage

--- src/modules/Main/pages/Main.js
+++ src/modules/Main/pages/Main.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Layout } from "../../../components/layout/Layout";
+import { Header } from "../../../components/Header";
+import { Layout } from "../../../components/Layout";
 import { loggedUser } from "../../../service/api";
+import { Home } from "@material-ui/icons";
 
 const Main = ({ history }) => {
   const [user, setUser] = useState({});
@@ -11,10 +13,12 @@ const Main = ({ history }) => {
   }, []);
 
   return (
-    <Layout history={history} titlePage={`Bem vindo ${user.name}`}>
-      <div>
-        <p>teste</p>
-      </div>
+    <Layout history={history} titlePage={"Home"}>
+      <Header
+        icon={<Home fontSize={"large"} />}
+        title={`Bem vindo, ${user.name}`}
+        description={"Olha como está nossa aplicação hoje"}
+      />
     </Layout>
   );
 };
