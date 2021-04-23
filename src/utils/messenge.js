633a7854979a820d1586d73b0850d3049f2e0a38
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-04-22 22:37:51 -0300

    feat: create error alert

--- src/utils/messenge.js
+++ src/utils/messenge.js
@@ -1,4 +1,13 @@
 import Swal from "sweetalert2";
+
 export const showMessage = (title, messenge) => {
   return Swal.fire(title, messenge, "success");
+};
+
+export const errorMessage = (title, messenge) => {
+  return Swal.fire({
+    icon: "error",
+    title: title,
+    text: messenge,
+  });
 };
